7db00ffae9bd83429fd0ef52cf7222000d90129a
chipgw <gw.chip.gw@gmail.com>
2015-09-21 20:22:19 -0500

    Start in the user's home dir by default.

--- depthview2/src/main.cpp
+++ depthview2/src/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QDir>
 #include "dvwindow.hpp"
 
 int main(int argc, char *argv[]) {
@@ -9,6 +10,10 @@ int main(int argc, char *argv[]) {
     fmt.setDepthBufferSize(24);
     fmt.setStencilBufferSize(8);
     QSurfaceFormat::setDefaultFormat(fmt);
+
+    /* If not started in a specific directory default to the user's home path. */
+    if(QDir::currentPath() == app.applicationDirPath())
+        QDir::setCurrent(QDir::homePath());
 
     DVWindow window;
     window.show();
