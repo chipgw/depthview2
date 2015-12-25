5af776415076a9210222273985eafc1c44b541d0
chipgw <gw.chip.gw@gmail.com>
2015-12-24 22:01:49 -0600

    Add descriptions to some DVQmlCommunication members.

--- depthview2/include/dvqmlcommunication.hpp
+++ depthview2/include/dvqmlcommunication.hpp
@@ -79,18 +79,29 @@ public:
 
     QStringList bookmarks() const;
 
+    /* Returns a list of strings describing mounted drives in the format: "mount path;display name". */
     QStringList getStorageDevicePaths() const;
 
+    /* Seriously, why doesn't QML have these capabilities somewhere? */
     Q_INVOKABLE bool fileExists(QString file) const;
     Q_INVOKABLE bool dirExists(QString dir) const;
 
+    /* Used by QML to convert local file strings to URLs and visa versa. */
     Q_INVOKABLE QUrl encodeURL(QString url) const;
     Q_INVOKABLE QString decodeURL(QUrl url) const;
 
+    /* Decrement the current history item and return its value. Only call when canGoBack is true. */
     Q_INVOKABLE QString goBack();
+    /* Increment the current history item and return its value. Only call when canGoBack is true. */
     Q_INVOKABLE QString goForward();
+
+    /* Called whenever the file browser changes directories. If value is empty or the current history item nothing happens.
+     * If it's the next one we go forward. If it's the previous one we go back. Otherwise we clear any forward history and add it at the end of the list. */
     Q_INVOKABLE void pushHistory(QString value);
+
+    /* Is there a value to go back to? */
     bool canGoBack() const;
+    /* Is there a value to go forward to? */
     bool canGoForward() const;
 
 signals:
