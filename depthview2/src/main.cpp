chipgw/depthview2

commit b4a48cf49b555e53e036c1c49ba51768f7afead4

Added support for file association on Windows.

depthview2/src/main.cpp
@@ -1,7 +1,11 @@
 #include <QApplication>
 #include <QDir>
+#include <QCommandLineParser>
+#include <QMessageBox>
 #include "version.hpp"
 #include "dvwindow.hpp"
+
+void registerFileTypes();
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -16,12 +20,88 @@ int main(int argc, char *argv[]) {
     fmt.setStencilBufferSize(8);
     QSurfaceFormat::setDefaultFormat(fmt);
 
+    QCommandLineParser parser;
+
+    /* TODO - More arguments. */
+    parser.addOptions({
+            {"register", QCoreApplication::translate("main", "")},
+            { {"f", "fullscreen"},
+              QCoreApplication::translate("main", "")},
+            { {"d", "startdir"},
+              QCoreApplication::translate("main", ""),
+              QCoreApplication::translate("main", "directory")},
+            { {"r", "renderer"},
+              QCoreApplication::translate("main", ""),
+              QCoreApplication::translate("main", "renderer")},
+        });
+
+    parser.parse(app.arguments());
+
+    if(parser.isSet("register")){
+        registerFileTypes();
+        return 0;
+    }
+
     /* If not started in a specific directory default to the user's home path. */
     if(QDir::currentPath() == app.applicationDirPath())
         QDir::setCurrent(QDir::homePath());
 
     DVWindow window;
     window.show();
+    window.doCommandLine(parser);
 
     return app.exec();
+}
+
+/* File association code. */
+
+#if defined(Q_OS_WIN32)
+/* Windows-specific code used for file association. */
+#include <Windows.h>
+#pragma comment(lib, "advapi32")
+
+void addRegistryEntry(const QString& path, const QString& value, QString& error){
+    /* TODO - handle errors properly. */
+    HKEY key;
+
+    LSTATUS result = RegCreateKeyExA(HKEY_CURRENT_USER, LPCSTR(path.toLocal8Bit().constData()), 0, nullptr,
+                                     REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, nullptr, &key, nullptr);
+
+    if (result == ERROR_SUCCESS) {
+        result = RegSetValueExA(key, nullptr, 0, REG_SZ, LPBYTE(value.toLocal8Bit().constData()), DWORD(value.size()));
+        if (result != ERROR_SUCCESS)
+            error += "<p>Error setting value for \"" + path + "\"!</p>";
+
+        RegCloseKey(key);
+    } else {
+        error += "<p>Error creating key \"" + path + "\"!</p>";
+    }
+}
+#endif
+
+void registerFileTypes() {
+    QString error;
+
+#if defined(Q_OS_WIN32)
+    QString progID = "chipgw.DepthView." + version::number.toString();
+
+    addRegistryEntry("Software\\Classes\\.jps", progID, error);
+    addRegistryEntry("Software\\Classes\\.pns", progID, error);
+
+    /* TODO - Add video formats. */
+
+    addRegistryEntry("Software\\Classes\\" + progID, "Stereo 3D Image", error);
+
+    QString command = "\"" + QDir::toNativeSeparators(QApplication::applicationFilePath()) + "\" \"%1\"";
+    addRegistryEntry("Software\\Classes\\" + progID + "\\shell\\open\\command", command, error);
+
+#else
+    /* TODO - make other platforms work. */
+    error = tr("File association is currently unsupported on your platform!");
+#endif
+
+    if (error.isNull())
+        QMessageBox::information(nullptr, QObject::tr("Success!"), QObject::tr("Successfully associated .jps and .pns files with DepthView."));
+    else
+        QMessageBox::warning(nullptr, QObject::tr("Error setting file association!"), error);
 }

depthview2/include/dvqmlcommunication.hpp
@@ -66,9 +66,6 @@ public:
     QStringList getPluginModes() const;
     QStringList getModes() const;
 
-    /* Parse command line arguments from QApplication. */
-    void doCommandLine(class DVFolderListing* folderListing);
-
     Q_INVOKABLE QString versionString();
     Q_INVOKABLE QString buildType();
     Q_INVOKABLE QString buildCompiler();

depthview2/include/dvwindow.hpp
@@ -21,6 +21,9 @@ public:
     DVWindow();
     ~DVWindow();
 
+    /* Parse command line arguments from QApplication. */
+    void doCommandLine(class QCommandLineParser& parser);
+
 public slots:
     void updateQmlSize();
 

depthview2/src/dvqmlcommunication.cpp
@@ -5,7 +5,6 @@
 #include <QDir>
 #include <QApplication>
 #include <QMessageBox>
-#include <QCommandLineParser>
 #include <QSettings>
 
 DVQmlCommunication::DVQmlCommunication(QWindow* parent, QSettings& s) : QObject(parent), settings(s), owner(parent) {
@@ -147,64 +146,6 @@ QStringList DVQmlCommunication::getModes() const {
                          << "Mono Left"
                          << "Mono Right"
                          << pluginModes;
-}
-
-void DVQmlCommunication::doCommandLine(DVFolderListing *folderListing) {
-    QCommandLineParser parser;
-
-    /* TODO - More arguments. */
-    parser.addOptions({
-            { {"f", "fullscreen"},
-              QCoreApplication::translate("main", "")},
-            { {"d", "startdir"},
-              QCoreApplication::translate("main", ""),
-              QCoreApplication::translate("main", "directory")},
-            { {"r", "renderer"},
-              QCoreApplication::translate("main", ""),
-              QCoreApplication::translate("main", "renderer")},
-        });
-
-    parser.parse(QApplication::arguments());
-
-    /* We use one string to hold all warning messages, so we only have to show one dialog. */
-    QString warning;
-
-    if(parser.isSet("f"))
-        setFullscreen(true);
-
-    if(parser.isSet("d") && !folderListing->initDir(parser.value("d")))
-        warning += tr("<p>Invalid directory \"%1\" passed to \"--startdir\" argument!</p>").arg(parser.value("d"));
-
-    if(parser.isSet("r")){
-        const QString &renderer = parser.value("r");
-
-        int mode = getModes().indexOf(renderer);
-
-        if(mode == -1)
-            warning += tr("<p>Invalid renderer \"%1\" passed to \"--renderer\" argument!</p>").arg(renderer);
-
-        if (mode >= DVDrawMode::Plugin) {
-            m_pluginMode = renderer;
-            m_drawMode = DVDrawMode::Plugin;
-        } else {
-            m_drawMode = DVDrawMode::Type(mode);
-        }
-    }
-
-    for (const QString& arg : parser.positionalArguments()) {
-        QFileInfo file(arg);
-
-        /* TODO - Check extension. */
-        if (file.exists()) {
-            folderListing->openFile(file);
-            break;
-        }
-    }
-
-    /* If there weren't any warnings we don't show the dialog. */
-    if(!warning.isEmpty())
-        /* TODO - Perhaps this should be done within QML? */
-        QMessageBox::warning(nullptr, tr("Invalid Command Line!"), warning);
 }
 
 QString DVQmlCommunication::versionString() {

depthview2/src/dvwindow.cpp
@@ -14,6 +14,8 @@
 #include <QOpenGLContext>
 #include <QOpenGLFunctions>
 #include <QPluginLoader>
+#include <QCommandLineParser>
+#include <QMessageBox>
 #include <QDir>
 
 /* Vertex attrib locations. */
@@ -44,8 +46,6 @@ public:
 DVWindow::DVWindow() : QOpenGLWindow(), settings(SETTINGS_ARGS), fboRight(nullptr), fboLeft(nullptr) {
     qmlCommunication = new DVQmlCommunication(this, settings);
     folderListing = new DVFolderListing(this, settings);
-
-    qmlCommunication->doCommandLine(folderListing);
 
     /* Use the class defined above. */
     qmlRenderControl = new RenderControl(this);
@@ -424,4 +424,46 @@ void DVWindow::unloadPlugins() {
 
     /* Clear the list. Not that it should be used anymore... */
     renderPlugins.clear();
+}
+
+void DVWindow::doCommandLine(QCommandLineParser& parser) {
+    /* We use one string to hold all warning messages, so we only have to show one dialog. */
+    QString warning;
+
+    if(parser.isSet("f"))
+        setWindowState(Qt::WindowFullScreen);
+
+    if(parser.isSet("d") && !folderListing->initDir(parser.value("d")))
+        warning += tr("<p>Invalid directory \"%1\" passed to \"--startdir\" argument!</p>").arg(parser.value("d"));
+
+    if(parser.isSet("r")){
+        const QString& renderer = parser.value("r");
+
+        int mode = qmlCommunication->getModes().indexOf(renderer);
+
+        if(mode == -1)
+            warning += tr("<p>Invalid renderer \"%1\" passed to \"--renderer\" argument!</p>").arg(renderer);
+
+        if (mode >= DVDrawMode::Plugin) {
+            qmlCommunication->setPluginMode(renderer);
+            qmlCommunication->setDrawMode(DVDrawMode::Plugin);
+        } else {
+            qmlCommunication->setDrawMode(DVDrawMode::Type(mode));
+        }
+    }
+
+    for (const QString& arg : parser.positionalArguments()) {
+        QFileInfo file(arg);
+
+        /* TODO - Check extension. */
+        if (file.exists()) {
+            folderListing->openFile(file);
+            break;
+        }
+    }
+
+    /* If there weren't any warnings we don't show the dialog. */
+    if(!warning.isEmpty())
+        /* TODO - Perhaps this should be done within QML? */
+        QMessageBox::warning(nullptr, tr("Invalid Command Line!"), warning);
 }

commit fb10f24e8b497960c22560e5f825d5542814f5a4

Fixed building on non-Windows platforms. (Broken in b4a48cf with file association support.)

depthview2/src/main.cpp
@@ -98,7 +98,7 @@ void registerFileTypes() {
 
 #else
     /* TODO - make other platforms work. */
-    error = tr("File association is currently unsupported on your platform!");
+    error = QObject::tr("File association is currently unsupported on your platform!");
 #endif
 
     if (error.isNull())