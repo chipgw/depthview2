chipgw/depthview2

commit 028e8b2af4535844a5d4464e48a7cc83f7369569

Store list of valid UI styles in C++.
Use this list to verify uiTheme value and to fill settings ComboBox.

depthview2/src/dvqmlcommunication.cpp
@@ -261,14 +261,17 @@ void DVQmlCommunication::setStartupFileBrowser(bool open) {
     }
 }
 
-QString DVQmlCommunication::uiTheme() {
+QString DVQmlCommunication::uiTheme() const {
     /* Either get it from settings, or get the one currently being used. */
     return settings.contains("ControlsTheme") ? settings.value("ControlsTheme").toString() : QQuickStyle::name();
 }
 
+QStringList DVQmlCommunication::uiThemes() const {
+    return {"Default", "Material", "Universal"};
+}
+
 void DVQmlCommunication::setUiTheme(QString theme) {
-    if (!settings.contains("ControlsTheme") || settings.value("ControlsTheme").toString() != theme) {
-        /* TODO - Verify as valid. */
+    if (!(settings.contains("ControlsTheme") || settings.value("ControlsTheme").toString() != theme) && uiThemes().contains(theme)) {
         settings.setValue("ControlsTheme", theme);
 
         emit uiThemeChanged();
@@ -287,5 +290,4 @@ void DVQmlCommunication::setUiTheme(QString theme) {
 void DVQmlCommunication::registerFileTypes() {
     fileassociation::registerFileTypes();
 }
-
 #endif

depthview2/include/dvqmlcommunication.hpp
@@ -35,6 +35,7 @@ class DVQmlCommunication : public QObject {
     Q_PROPERTY(bool startupFileBrowser READ startupFileBrowser WRITE setStartupFileBrowser NOTIFY startupFileBrowserChanged)
 
     Q_PROPERTY(QString uiTheme READ uiTheme WRITE setUiTheme NOTIFY uiThemeChanged)
+    Q_PROPERTY(QStringList uiThemes READ uiThemes)
 
 public:
     /* Settings can be set from DVWindow. */
@@ -90,7 +91,8 @@ public:
     bool startupFileBrowser() const;
     void setStartupFileBrowser(bool open);
 
-    QString uiTheme();
+    QString uiTheme() const;
+    QStringList uiThemes() const;
     void setUiTheme(QString theme);
 
 #ifdef DV_FILE_ASSOCIATION

depthview2/qml/SettingsWindow.qml
@@ -126,7 +126,7 @@ Popup {
                             ComboBox {
                                 id: uiThemeComboBox
 
-                                model: ["Default", "Material", "Universal"]
+                                model: DepthView.uiThemes
                             }
                         }
                         Button {

commit 78e07351448c11054c28b9f2a69ae2b1730ba388

Fixed issue with setting uiTheme introduced in 028e8b2.

depthview2/src/dvqmlcommunication.cpp
@@ -271,7 +271,7 @@ QStringList DVQmlCommunication::uiThemes() const {
 }
 
 void DVQmlCommunication::setUiTheme(QString theme) {
-    if (!(settings.contains("ControlsTheme") || settings.value("ControlsTheme").toString() != theme) && uiThemes().contains(theme)) {
+    if ((!settings.contains("ControlsTheme") || settings.value("ControlsTheme").toString() != theme) && uiThemes().contains(theme)) {
         settings.setValue("ControlsTheme", theme);
 
         emit uiThemeChanged();