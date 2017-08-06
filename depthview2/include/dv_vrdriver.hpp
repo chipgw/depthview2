0a23232a5fd1e0d4b6c4af0043a7139b07068db5
chipgw <gw.chip.gw@gmail.com>
2017-08-05 19:06:34 -0700

    Fix last commit not building on Windows.

--- depthview2/include/dv_vrdriver.hpp
+++ depthview2/include/dv_vrdriver.hpp
@@ -13,7 +13,7 @@ protected:
     DV_VRDriver(DVWindow* w);
 
 public:
-    virtual ~DV_VRDriver() = 0;
+    virtual ~DV_VRDriver() = default;
     virtual bool render() = 0;
 
     virtual void frameSwapped() = 0;
